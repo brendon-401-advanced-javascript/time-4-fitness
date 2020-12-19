746ae1d096f9e0ffd9f9ec0dd025baf7fef009da
BrendonLH <Brendonhampton1988@gmail.com>
2020-12-19 15:31:21 -0800

    just about finished the basic functionality of it

---
+++ src/components/timer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useCountdownTimer } from 'use-countdown-timer';
+import {Button} from 'react-bootstrap';
+
+
+
+export default function Timer (props) {
+
+      const { countdown, start, reset, pause, isRunning } = useCountdownTimer({
+        timer: 1000*60,
+      });
+    
+      return (
+        <React.Fragment>
+            <div className='timer'>
+                <h1>{countdown}</h1>
+                <Button onClick={reset} variant="outline-warning">Reset</Button>
+                {isRunning ? (
+                    <Button onClick={pause} variant="outline-danger">Stop</Button>
+                ) : (
+                    <Button onClick={start} variant="outline-success">Start</Button>
+                )}
+            </div>
+        </React.Fragment>
+      );
+}
+
